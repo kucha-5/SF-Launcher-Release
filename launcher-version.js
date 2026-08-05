ced68eda11c2f6b9b4dfeb5837e287456bc5e4b1
kucha-5 <yuyangchen2014@gmail.com>
2026-08-04 19:59:38 -0700

    Update launcher-version.js

--- launcher-version.js
+++ launcher-version.js
@@ -1,6 +1,6 @@
 window.SF_LAUNCHER_VERSION = {
-  "latestVersion": "49.19.7",
-  "downloadUrl": "https://github.com/kucha-5/Project-Zero-releases/releases/download/launcher-49.19.7/SF_Launcher_49.19.7_x64.zip",
+  "latestVersion": "49.19.8",
+  "downloadUrl": "https://github.com/kucha-5/Project-Zero-releases/releases/download/launcher-49.19.8/SF_Launcher_49.19.8_x64.zip",
   "sha256": "ad4f26c4179739bf41de29942876baefd8f6c7856f00f5b5d454168f530d1dc1",
   "mandatory": false,
   "updateLogZh": ["修复版本检查失败弹窗", "GitHub不可用时自动回退到内置版本信息", "保留JSON、JS备用清单和网络重试"],
